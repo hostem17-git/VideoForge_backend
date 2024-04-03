c039a663b2d5ae0799053767a5b87d9107b01a1b
hostem17 <vjjais17@gmail.com>
2024-04-03 05:58:04 +0530

    DB schema definitions added

--- db/index.js
+++ db/index.js
@@ -1,9 +1,70 @@
-const { default: mongoose } = require("mongoose");
+const { default: mongoose, Mongoose } = require("mongoose");
 
 // Create and export db schemas
 mongoose.connect(process.env.MONGODB_URL_DEV);
 
 // Admin Schemna 
 const AdminSchema = new mongoose.Schema({
- u
+    username: String,
+    encryptedPassword: String,
+    DateCreated: {
+        type: Date,
+        default: () => Date.now()
+    }
 })
+
+// InfluencerSchema
+const InfluencerSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    encryptedPassword: String,
+    createdJobs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Job'
+    }]
+})
+
+// userSchame
+const UserSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    encryptedPassword: String,
+    JobsTaken: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Job'
+    }]
+})
+
+const JobSchema = new mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Influencer'
+    },
+    users: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Influencer'
+    }],
+    files: [String],
+    JobTitle: String,
+    CreatedDate: {
+        type: Date,
+        default: () => Date.now()
+    },
+    StartDate: Date,
+    DueDate: Date,
+    ClosedDate: Date,
+    CompletedDate: Date,
+    Stage: {
+        type: String,
+        enum: ["new", "started", "closed"]
+    }
+})
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const Influencer = mongoose.model('Influencer', InfluencerSchema);
+const User = mongoose.model('User', UserSchema);
+const Job = mongoose.model('Job', JobSchema);
+
+module.exports = {
+    Admin, Influencer, User, Job
+}
